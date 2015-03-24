61578fb285fe412fda2445f4ec2821731f79f0dd
Matthew <9029537@gmail.com>
2015-03-24 10:59:38 -0700

    --dry-run

--- web/logic.js
+++ web/logic.js
@@ -1,69 +1,80 @@
-_ = require('underscore')
+function initSolver() {
 
-DIRECTIONS = [
-  [0, -1],
-  [-1, 0],
-  [0, 1],
-  [1, 0]
-];
+  _ = require("underscore");
 
-marked = [];
+  DIRECTIONS = [
+    [0, -1],
+    [-1, 0],
+    [0, 1],
+    [1, 0]
+  ];
 
-function contains(coordinate, board) {
-  return _.where(board, coordinate).length > 0
-};
+  function contains(coordinate, board) {
+    return _.where(board, coordinate).length > 0
+  };
 
-function find_neighbour(coordinate, board) {
-  neighbours = [];
-  _.each(DIRECTIONS, function(d) {
-    new_col = coordinate[0] + d[0];
-    new_row = coordinate[1] + d[1];
-    new_coordinate = [new_col, new_row]
-    if (
-      (contains(new_coordinate, board)) &&
-      !(contains(new_coordinate, marked))
-    ) {
-      neighbours.push(new_coordinate)
-    }
-  });
-  return neighbours;
-};
-
-function game_ended(marked, board) {
-  return marked.length == board.length
-};
-
-function mark_coordinate(coordinate) {
-  if (!contains(coordinate, marked)) {
-    marked.push(coordinate);
-  }
-};
-
-function calc_moves(board) {
-  while (!game_ended(marked, board)) {
-    _.each(board, function(coordinate) {
-      neighbours = find_neighbour(coordinate);
-      count = neighbours.length;
-      if (count == 2 || count == 4 || count == 0) {
-        mark_coordinate(coordinate);
-      } else if (count == 1) {
-        if (find_neighbour(neighbours[0]).length == 1) {
-          mark_coordinate(coordinate)
-        }
+  function find_neighbour(coordinate, board, marked) {
+    neighbours = [];
+    DIRECTIONS.forEach(function(d) {
+      new_col = coordinate[0] + d[0];
+      new_row = coordinate[1] + d[1];
+      new_coordinate = [new_col, new_row]
+      if (
+        contains(new_coordinate, board) &&
+        !contains(new_coordinate, marked)
+      ) {
+        neighbours.push(new_coordinate)
       }
-    })
-  }
-  console.log(marked);
-  return marked;
+    });
+    return neighbours;
+  };
+
+  function game_ended(marked, board) {
+    return marked.length == board.length;
+  };
+
+  function mark_coordinate(coordinate, marked) {
+    if (!contains(coordinate, marked)) {
+      marked.push(coordinate);
+    }
+  };
+
+  this.calc_move = function(board) {
+
+    var marked = [];
+
+
+    board.sort(function(a, b) {
+      if (a[1] === b[1]) {
+        return 0;
+      } else {
+        return (a[1] < b[1]) ? -1 : 1;
+      }
+    });
+
+    while (!game_ended(marked, board)) {
+
+      board.forEach(function(coordinate) {
+        neighbours = find_neighbour(coordinate, board, marked);
+        count = neighbours.length;
+        if (count == 2 || count == 4 || count == 0) {
+          if (count == 0) {}
+          mark_coordinate(coordinate, marked);
+        } else if (count == 1) {
+          if (find_neighbour(neighbours[0], board, marked).length == 1) {
+            mark_coordinate(coordinate, marked);
+          }
+        }
+      })
+    }
+    return marked;
+  };
+
+  return this;
 };
 
-function sortBoard(a, b) {
-  if (a[1] === b[1]) {
-    return 0;
-  } else {
-    return (a[1] < b[1]) ? -1 : 1;
-  }
-};
+/*
+solver = new initSolver();
 
 board = [
   [0, 1],
@@ -71,8 +82,7 @@ board = [
   [1, 1],
   [2, 0],
   [2, 1]
-]
+];
 
-board.sort(sortBoard);
-
-calc_moves(board);
+console.log(solver.calc_move(board))
+*/
